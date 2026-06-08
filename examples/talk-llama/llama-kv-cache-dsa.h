84bd03a438454a82150853dce83818013c6609d2
Georgi Gerganov <ggerganov@gmail.com>
2026-06-08 12:55:06 +0300

    talk-llama : sync llama.cpp

--- examples/talk-llama/llama-kv-cache-dsa.h
+++ examples/talk-llama/llama-kv-cache-dsa.h
@@ -5,32 +5,31 @@
 #include <vector>
 
 //
-// llama_kv_cache_iswa
+// llama_kv_cache_dsa
 //
 
-// utilizes two instances of llama_kv_cache
-//   the first instance is for the non-SWA layers of the model and the second instance is for the SWA layers
+// utilizes two instances of llama_kv_cache:
+// - the first instance is for caching key tensors of the model,
+// - the second instance is for caching lightning indexer key tensors
 
-class llama_kv_cache_iswa : public llama_memory_i {
+class llama_kv_cache_dsa : public llama_memory_i {
 public:
-    llama_kv_cache_iswa(
+    llama_kv_cache_dsa(
             const llama_model & model,
                     ggml_type   type_k,
                     ggml_type   type_v,
                          bool   v_trans,
                          bool   offload,
-                         bool   swa_full,
                          bool   unified,
                      uint32_t   kv_size,
                      uint32_t   n_seq_max,
-                     uint32_t   n_ubatch,
                      uint32_t   n_pad,
-               llama_memory_t   mem_other,
+                     uint32_t   n_swa,
+               llama_swa_type   swa_type,
         const layer_filter_cb & filter,
-        const  layer_reuse_cb & reuse,
-        const  layer_share_cb & share);
+        const  layer_reuse_cb & reuse);
 
-    ~llama_kv_cache_iswa() = default;
+    ~llama_kv_cache_dsa() = default;
 
     //
     // llama_memory_i
@@ -66,46 +65,46 @@ public:
     void state_read (llama_io_read_i  & io, llama_seq_id seq_id = -1, llama_state_seq_flags flags = 0) override;
 
     //
-    // llama_kv_cache_iswa specific API
+    // llama_kv_cache_dsa specific API
     //
 
-    llama_kv_cache * get_base() const;
-    llama_kv_cache * get_swa () const;
+    llama_kv_cache * get_mla() const;
+    llama_kv_cache * get_lid() const;
 
 private:
-    const llama_hparams & hparams;
+    // we keep indexer KV cache hparams instance here as llama_kv_cache stores only reference to it
+    llama_hparams hparams_lid;
+    const uint32_t n_stream  = 1;
 
-    const bool unified;
-
-    std::unique_ptr<llama_kv_cache> kv_base;
-    std::unique_ptr<llama_kv_cache> kv_swa;
+    std::unique_ptr<llama_kv_cache> kv_mla;
+    std::unique_ptr<llama_kv_cache> kv_lid;
 };
 
-class llama_kv_cache_iswa_context : public llama_memory_context_i {
+class llama_kv_cache_dsa_context : public llama_memory_context_i {
 public:
     using slot_info_vec_t = llama_kv_cache::slot_info_vec_t;
 
     // used for errors
-    llama_kv_cache_iswa_context(llama_memory_status status);
+    llama_kv_cache_dsa_context(llama_memory_status status);
 
     // used to create a full-cache context
-    llama_kv_cache_iswa_context(
-            llama_kv_cache_iswa * kv);
+    llama_kv_cache_dsa_context(
+            llama_kv_cache_dsa * kv);
 
     // used to create an update context
-    llama_kv_cache_iswa_context(
-            llama_kv_cache_iswa * kv,
+    llama_kv_cache_dsa_context(
+            llama_kv_cache_dsa * kv,
             llama_context * lctx,
             bool optimize);
 
     // used to create a batch processing context from a batch
-    llama_kv_cache_iswa_context(
-            llama_kv_cache_iswa * kv,
+    llama_kv_cache_dsa_context(
+            llama_kv_cache_dsa * kv,
             slot_info_vec_t sinfos_base,
-            slot_info_vec_t sinfos_swa,
+            slot_info_vec_t sinfos_ik,
             std::vector<llama_ubatch> ubatches);
 
-    virtual ~llama_kv_cache_iswa_context();
+    virtual ~llama_kv_cache_dsa_context();
 
     //
     // llama_memory_context_i
@@ -118,22 +117,22 @@ public:
     const llama_ubatch & get_ubatch() const override;
 
     //
-    // llama_kv_cache_iswa_context specific API
+    // llama_kv_cache_dsa_context specific API
     //
 
-    const llama_kv_cache_context * get_base() const;
-    const llama_kv_cache_context * get_swa()  const;
+    const llama_kv_cache_context * get_mla() const;
+    const llama_kv_cache_context * get_lid()  const;
 
 private:
-    //llama_kv_cache_iswa * kv;
+    //llama_kv_cache_dsa * kv;
 
     // the index of the next ubatch to process
     size_t i_next = 0;
 
     std::vector<llama_ubatch> ubatches;
 
-    const llama_memory_context_ptr ctx_base;
-    const llama_memory_context_ptr ctx_swa;
+    const llama_memory_context_ptr ctx_mla;
+    const llama_memory_context_ptr ctx_lid;
 
     const llama_memory_status status;
 };
--- examples/talk-llama/models/mellum.cpp
+++ examples/talk-llama/models/mellum.cpp
@@ -1,28 +1,43 @@
 #include "models.h"
 
-void llama_model_qwen3moe::load_arch_hparams(llama_model_loader & ml) {
-    ml.get_key(LLM_KV_EXPERT_FEED_FORWARD_LENGTH,  hparams.n_ff_exp, false);
+void llama_model_mellum::load_arch_hparams(llama_model_loader & ml) {
     ml.get_key(LLM_KV_ATTENTION_LAYERNORM_RMS_EPS, hparams.f_norm_rms_eps);
+    ml.get_key(LLM_KV_EXPERT_FEED_FORWARD_LENGTH,  hparams.n_ff_exp);
+    ml.get_key(LLM_KV_ATTENTION_SLIDING_WINDOW,    hparams.n_swa, false);
+
+    if (hparams.n_swa > 0) {
+        hparams.swa_type = LLAMA_SWA_TYPE_STANDARD;
+
+        uint32_t swa_period = 4;
+        const auto res = ml.get_key_or_arr(LLM_KV_ATTENTION_SLIDING_WINDOW_PATTERN, swa_period, false);
+        if (res) {
+            hparams.set_swa_pattern(swa_period);
+        } else {
+            ml.get_key_or_arr(LLM_KV_ATTENTION_SLIDING_WINDOW_PATTERN, hparams.is_swa_impl, hparams.n_layer());
+        }
+
+        hparams.rope_freq_base_train_swa  = hparams.rope_freq_base_train;
+        hparams.rope_freq_scale_train_swa = hparams.rope_freq_scale_train;
+
+        ml.get_key(LLM_KV_ROPE_FREQ_BASE_SWA, hparams.rope_freq_base_train_swa, false);
+    } else {
+        hparams.swa_type = LLAMA_SWA_TYPE_NONE;
+    }
 
     switch (hparams.n_layer()) {
-        case 48: type = LLM_TYPE_30B_A3B; break;
-        case 94: type = LLM_TYPE_235B_A22B; break;
+        case 28: type = LLM_TYPE_12B_A2_5B; break;
         default: type = LLM_TYPE_UNKNOWN;
     }
 }
 
-void llama_model_qwen3moe::load_arch_tensors(llama_model_loader &) {
+void llama_model_mellum::load_arch_tensors(llama_model_loader &) {
     LLAMA_LOAD_LOCALS;
 
     tok_embd = create_tensor(tn(LLM_TENSOR_TOKEN_EMBD, "weight"), {n_embd, n_vocab}, 0);
 
     // output
     output_norm = create_tensor(tn(LLM_TENSOR_OUTPUT_NORM, "weight"), {n_embd}, 0);
-    output      = create_tensor(tn(LLM_TENSOR_OUTPUT,      "weight"), {n_embd, n_vocab}, TENSOR_NOT_REQUIRED);
-    // if output is NULL, init from the input tok embed
-    if (output == NULL) {
-        output = create_tensor(tn(LLM_TENSOR_TOKEN_EMBD, "weight"), {n_embd, n_vocab}, TENSOR_DUPLICATED);
-    }
+    output      = create_tensor(tn(LLM_TENSOR_OUTPUT,      "weight"), {n_embd, n_vocab}, 0);
 
     for (int i = 0; i < n_layer; ++i) {
         auto & layer = layers[i];
@@ -40,13 +55,12 @@ void llama_model_qwen3moe::load_arch_tensors(llama_model_loader &) {
         layer.ffn_gate_inp = create_tensor(tn(LLM_TENSOR_FFN_GATE_INP, "weight", i), {n_embd, n_expert}, 0);
 
         if (n_expert == 0) {
-            throw std::runtime_error("n_expert must be > 0 for QWEN3MOE");
+            throw std::runtime_error("n_expert must be > 0 for Mellum");
         }
         if (n_expert_used == 0) {
-            throw std::runtime_error("n_expert_used must be > 0 for QWEN3MOE");
+            throw std::runtime_error("n_expert_used must be > 0 for Mellum");
         }
 
-        // MoE branch
         const int64_t n_ff_exp = hparams.n_ff_exp ? hparams.n_ff_exp : n_ff / n_expert_used;
 
         layer.ffn_gate_exps = create_tensor(tn(LLM_TENSOR_FFN_GATE_EXPS, "weight", i), {  n_embd, n_ff_exp, n_expert}, 0);
@@ -55,11 +69,15 @@ void llama_model_qwen3moe::load_arch_tensors(llama_model_loader &) {
     }
 }
 
-std::unique_ptr<llm_graph_context> llama_model_qwen3moe::build_arch_graph(const llm_graph_params & params) const {
-    return std::make_unique<graph>(*this, params);
+std::unique_ptr<llm_graph_context> llama_model_mellum::build_arch_graph(const llm_graph_params & params) const {
+    if (hparams.swa_type == LLAMA_SWA_TYPE_STANDARD) {
+        return std::make_unique<graph<true>>(*this, params);
+    }
+    return std::make_unique<graph<false>>(*this, params);
 }
 
-llama_model_qwen3moe::graph::graph(const llama_model & model, const llm_graph_params & params) : llm_graph_context(params) {
+template <bool iswa>
+llama_model_mellum::graph<iswa>::graph(const llama_model & model, const llm_graph_params & params) : llm_graph_context(params) {
     const int64_t n_embd_head = hparams.n_embd_head_v();
 
     GGML_ASSERT(n_embd_head == hparams.n_embd_head_k());
@@ -73,7 +91,14 @@ llama_model_qwen3moe::graph::graph(const llama_model & model, const llm_graph_pa
     // inp_pos - contains the positions
     ggml_tensor * inp_pos = build_inp_pos();
 
-    auto * inp_attn = build_attn_inp_kv();
+    using inp_attn_type = std::conditional_t<iswa, llm_graph_input_attn_kv_iswa, llm_graph_input_attn_kv>;
+    inp_attn_type * inp_attn = nullptr;
+
+    if constexpr (iswa) {
+        inp_attn = build_attn_inp_kv_iswa();
+    } else {
+        inp_attn = build_attn_inp_kv();
+    }
 
     ggml_tensor * inp_out_ids = build_inp_out_ids();
 
@@ -82,7 +107,7 @@ llama_model_qwen3moe::graph::graph(const llama_model & model, const llm_graph_pa
 
         // norm
         cur = build_norm(inpL,
-                model.layers[il].attn_norm, NULL,
+                model.layers[il].attn_norm, nullptr,
                 LLM_NORM_RMS, il);
         cb(cur, "attn_norm", il);
 
@@ -92,23 +117,42 @@ llama_model_qwen3moe::graph::graph(const llama_model & model, const llm_graph_pa
             auto [Qcur, Kcur, Vcur] = build_qkv(model.layers[il], cur,
                     n_embd_head, n_head, n_head_kv, il);
 
-            Qcur = build_norm(Qcur, model.layers[il].attn_q_norm, NULL, LLM_NORM_RMS, il);
+            Qcur = build_norm(Qcur, model.layers[il].attn_q_norm, nullptr, LLM_NORM_RMS, il);
             cb(Qcur, "Qcur_normed", il);
 
-            Qcur = ggml_rope_ext(
+            Kcur = build_norm(Kcur, model.layers[il].attn_k_norm, nullptr, LLM_NORM_RMS, il);
+            cb(Kcur, "Kcur_normed", il);
+
+            const bool is_swa = hparams.is_swa(il);
+
+            if (is_swa) {
+                // For sliding window layers, use regular rope with no yarn rope scaling.
+                // This is achieved here by setting freq_scale and attn_factor to 1.
+                // We also set ext_factor to 0 to avoid a few unnecessary computations.
+                Qcur = ggml_rope_ext(
+                    ctx0, Qcur, inp_pos, nullptr,
+                    n_rot, rope_type, n_ctx_orig, freq_base, 1.0,
+                    0.0, 1.0, beta_fast, beta_slow
+                    );
+
+                Kcur = ggml_rope_ext(
+                    ctx0, Kcur, inp_pos, nullptr,
+                    n_rot, rope_type, n_ctx_orig, freq_base, 1.0,
+                    0.0, 1.0, beta_fast, beta_slow
+                    );
+            } else {
+                Qcur = ggml_rope_ext(
                     ctx0, Qcur, inp_pos, nullptr,
                     n_rot, rope_type, n_ctx_orig, freq_base, freq_scale,
                     ext_factor, attn_factor, beta_fast, beta_slow
                     );
 
-            Kcur = build_norm(Kcur, model.layers[il].attn_k_norm, NULL, LLM_NORM_RMS, il);
-            cb(Kcur, "Kcur_normed", il);
-
-            Kcur = ggml_rope_ext(
+                Kcur = ggml_rope_ext(
                     ctx0, Kcur, inp_pos, nullptr,
                     n_rot, rope_type, n_ctx_orig, freq_base, freq_scale,
                     ext_factor, attn_factor, beta_fast, beta_slow
                     );
+            }
 
             cb(Qcur, "Qcur", il);
             cb(Kcur, "Kcur", il);
@@ -125,9 +169,9 @@ llama_model_qwen3moe::graph::graph(const llama_model & model, const llm_graph_pa
         ggml_tensor * ffn_inp = ggml_add(ctx0, cur, inpSA);
         cb(ffn_inp, "ffn_inp", il);
 
-        // MoE branch
+        // MoE
         cur = build_norm(ffn_inp,
-                model.layers[il].ffn_norm, NULL,
+                model.layers[il].ffn_norm, nullptr,
                 LLM_NORM_RMS, il);
         cb(cur, "ffn_norm", il);
 
@@ -151,6 +195,7 @@ llama_model_qwen3moe::graph::graph(const llama_model & model, const llm_graph_pa
         cur = moe_out;
 
         cur = ggml_add(ctx0, cur, ffn_inp);
+        cb(cur, "ffn_out", il);
 
         cur = build_cvec(cur, il);
         cb(cur, "l_out", il);
@@ -161,7 +206,7 @@ llama_model_qwen3moe::graph::graph(const llama_model & model, const llm_graph_pa
     cur = inpL;
 
     cur = build_norm(cur,
-            model.output_norm, NULL,
+            model.output_norm, nullptr,
             LLM_NORM_RMS, -1);
 
     cb(cur, "result_norm", -1);
@@ -175,3 +220,6 @@ llama_model_qwen3moe::graph::graph(const llama_model & model, const llm_graph_pa
 
     ggml_build_forward_expand(gf, cur);
 }
+
+template struct llama_model_mellum::graph<false>;
+template struct llama_model_mellum::graph<true>;
